cb155affdeb22b1b5f2665e93fca1c51c91b9553
Air-bush <burda.adam@icloud.com>
2025-10-05 22:33:16 +0200

    Some cpp stuff

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,31 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+double power(int base, int exponent) {
+    if (exponent == 0) {
+        return 1;
+    }
+    else if (exponent == 1) {
+        return base;
+    }
+
+    double result = base;
+    for (int i = 0; i < exponent - 1; i++) {
+        result *= base;
+    }
+    return result;
+}
+
+void calculate_power() {
+    int base, exponent;
+    cout << "Enter base: ";
+    cin >> base;
+    cout << "Enter exponent: ";
+    cin >> exponent;
+    cout << power(base, exponent) << endl;
+}
+
 int main() {
-    int pizzas;
-    cin >> pizzas;
-    cout << pizzas << " pizzas ordered." << endl;
+    calculate_power;
     return 0;
 }
