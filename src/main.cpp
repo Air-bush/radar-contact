942e490ab123b8301b881531a96a44eee3dbec51
Air-bush <burda.adam@icloud.com>
2025-10-06 22:18:24 +0200

    Added basic window init

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <raylib.h>
 
+using std::cout, std::endl;
+
 int main() {
+    InitWindow(800, 600, "Radar Contact");
+    cout << "Ready: " << IsWindowReady() << endl;
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(Color{20, 20, 25, 255});
+        EndDrawing();
+    }
+    CloseWindow();
     return 0;
 }
